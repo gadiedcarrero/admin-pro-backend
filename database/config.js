4ad8531a313f2cd2dd577bda0b2ae6f350a64403
Gadied Carrero <gadiedcarrero@gmail.com>
2020-10-19 15:52:57 -0500

    CORS y Express - primer commit

---
+++ database/config.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+
+const dbConnection = async() => {
+
+    try {
+
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('BD Online');
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de cargar la BD');
+    }
+
+
+
+}
+
+module.exports = {
+    dbConnection
+}
